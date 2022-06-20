8eff08d11a452e75d6c90ed98ddad0b147212a94
Panos-1993 <panoslampros@icloud.com>
2022-06-20 17:57:39 +0300

    commit

--- script/scripts.js
+++ script/scripts.js
@@ -20,7 +20,7 @@ function myFunctionCapitalGearing() {
     }
     });
 
-    function myFunctionInterestCover() {
+function myFunctionInterestCover() {
     var x7, x8;
     x7 = document.getElementById("OperatingProfit").value;
     x8 = document.getElementById("FinanceCost").value;
@@ -41,3 +41,67 @@ function myFunctionCapitalGearing() {
     myFunctionInterestCover();
     }
     });
+function myFunctionInventoryDays() {
+      var x9, x10;
+      x9 = document.getElementById("Inventory").value;
+      x10 = document.getElementById("CoS").value;
+      console.log(x9 + x10);
+      if (x9 >= 0 && x10 >= 0) {
+      var result = (Number(x9) / Number(x10)) * 365;
+      console.log(result);
+      document.getElementById("resultInventoryDays").innerHTML = result;
+      document.getElementById("rresultInventoryDays").style.backgroundColor = "green";
+      return result;
+    } else {
+      document.getElementById("resultInventoryDays").style.backgroundColor = "red";
+      }
+  }
+  var wage = document.getElementById("CoS");
+  wage.addEventListener("keydown", function (e) {
+      if (e.key === "Enter") {  //checks whether the pressed key is "Enter"
+          myFunction();
+      }
+    });
+
+function myFunctionPayableDays() {
+      var x11, x12;
+      x11 = document.getElementById("Payables").value;
+      x12 = document.getElementById("CreditPurchases").value;
+      console.log(x11 + x12);
+      if (x11 >= 0 && x12 >= 0){
+      var result = (Number(x11) / Number(x12)) * 365;
+      console.log(result);
+      document.getElementById("resultPayableDays").innerHTML = result;
+      document.getElementById("resultPayableDays").style.backgroundColor = "green";
+      return result;
+      } else {
+      document.getElementById("resultPayableDays").style.backgroundColor = "red";
+      }
+      }
+      var wage = document.getElementById("CreditPurchases");
+      wage.addEventListener("keydown", function (e) {
+      if (e.key === "Enter") {  //checks whether the pressed key is "Enter"
+      myFunctionPayableDays();
+      }
+      });
+function myFunctionReceivablesDays() {
+        var x13, x14;
+        x13 = document.getElementById("Receivables").value;
+        x14 = document.getElementById("CreditSales").value;
+        console.log(x13 + x14);
+        if (x13 >= 0 && x14 >= 0){
+        var result = (Number(x13) / Number(x14)) * 365;
+        console.log(result);
+        document.getElementById("resultReceivablesDays").innerHTML = result;
+        document.getElementById("resultReceivablesDays").style.backgroundColor = "green";
+        return result;
+        } else {
+        document.getElementById("resultReceivablesDays").style.backgroundColor = "red";
+        }
+        }
+        var wage = document.getElementById("CreditSales");
+        wage.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {  //checks whether the pressed key is "Enter"
+        myFunctionReceivablesDays();
+        }
+      });
